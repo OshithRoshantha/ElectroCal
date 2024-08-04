45b4af9ce6aa4717dfedd052a711e6ac60dc526d
Oshith Roshantha <oedirisuriya@gmail.com>
2024-08-04 13:29:17 +0530

    'intialization'

---
+++ ElectroCalc/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ElectroCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Tahoma", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(25, 164, 212);
+            label1.Location = new Point(124, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(196, 45);
+            label1.TabIndex = 0;
+            label1.Text = "ElectroCalc";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(25, 164, 212);
+            label2.Location = new Point(63, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(325, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Calculate Your Monthly Electricity Bill ";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(25, 164, 212);
+            label3.Location = new Point(166, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 25);
+            label3.TabIndex = 2;
+            label3.Text = "with Ease!";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(88, 360);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Username";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(92, 447);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 23);
+            label5.TabIndex = 4;
+            label5.Text = "Password";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Light", 7F);
+            label6.ForeColor = SystemColors.ActiveCaptionText;
+            label6.Location = new Point(92, 422);
+            label6.Name = "label6";
+            label6.Size = new Size(177, 15);
+            label6.TabIndex = 5;
+            label6.Text = "The Username you registered with";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Light", 7F);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(92, 509);
+            label7.Name = "label7";
+            label7.Size = new Size(239, 15);
+            label7.TabIndex = 6;
+            label7.Text = "The password you specified during registration";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(25, 164, 212);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(88, 547);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 36);
+            button1.TabIndex = 7;
+            button1.Text = "Secure Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Violet;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(227, 547);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 36);
+            button2.TabIndex = 8;
+            button2.Text = "Register";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(92, 392);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(257, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(92, 479);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '•';
+            textBox2.Size = new Size(257, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI Semibold", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(50, 184);
+            label8.Name = "label8";
+            label8.Size = new Size(336, 17);
+            label8.TabIndex = 11;
+            label8.Text = "Are you curious about how much your electricity bill ?";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI Semibold", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(20, 203);
+            label9.Name = "label9";
+            label9.Size = new Size(407, 17);
+            label9.TabIndex = 12;
+            label9.Text = "Our Electric Bill Calculator is here to help you estimate your costs ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI Semibold", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(147, 222);
+            label10.Name = "label10";
+            label10.Size = new Size(148, 17);
+            label10.TabIndex = 13;
+            label10.Text = "quickly and accurately. ";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.FromArgb(25, 164, 212);
+            label11.Location = new Point(78, 250);
+            label11.Name = "label11";
+            label11.Size = new Size(281, 25);
+            label11.TabIndex = 14;
+            label11.Text = "Try Now! Our tool makes it easy.";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(432, 703);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximumSize = new Size(450, 750);
+            MinimumSize = new Size(450, 750);
+            Name = "Form1";
+            Text = "ElectroCalc";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
